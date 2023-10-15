f2b061a97da42d139ed194e25ce2a81c6e34871a
유정한 <you.picked@toss.im>
2023-10-15 22:29:39 +0900

    스프링 기초 세팅 및 회원 MemberService 구현

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 동시성 이슈로 concurrency hashMap을 써야함
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
